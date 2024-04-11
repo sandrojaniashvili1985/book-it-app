d6b701e0f80faf117186218452ad5f297e541b42
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-11 21:21:33 +0300

    fdsgf

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,4 +1,4 @@
-import Heading from "@/components/ui/Heading";
+import Heading from "../components/ui/Heading";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
