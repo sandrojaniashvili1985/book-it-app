e810fe6cc5c498e1c4ac5cf19fcd02ae80f7e713
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-13 01:48:24 +0300

    dfsd

--- api/server.ts
+++ api/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://book-it-app-client.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
@@ -21,24 +21,6 @@ app.use(cors());
 app.use(morgan("tiny"));
 
 app.use("/api/hotels/uploads", express.static(__dirname + "\\uploads"));
-
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "x-client-key",
-    "x-client-token",
-    "x-client-secret",
-    "Authorization"
-  );
-  next();
-});
 
 app.use(router);
 
