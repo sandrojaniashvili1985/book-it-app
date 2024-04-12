b9fa7ce1f369825f018ae8f11c982ebb9c68f051
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-13 02:48:15 +0300

    dfsd

--- api/server.ts
+++ api/server.ts
@@ -17,6 +17,17 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://book-it-app-client.vercel.app"
+  );
+  // Adjust other CORS headers as needed
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(morgan("tiny"));
 
 app.use("/api/hotels/uploads", express.static(__dirname + "\\uploads"));
