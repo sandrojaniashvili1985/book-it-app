f11e6b8d4c4750a04bad8b7d680b8fcceb9c17c7
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-13 03:04:58 +0300

    dfsd

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,13 @@ import { route } from "./routes";
 import "./lib/i18n";
 import "./index.css";
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 
 const routes = createBrowserRouter(route);
 
 // axios.defaults.baseURL = "https://book-it-app-six.vercel.app";
 // axios.defaults.baseURL = "http://localhost:5000";
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
